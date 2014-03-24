f9cecc02b5d07498e7e491e17b6962773af054b6
unknown <Songfan@.(none)>
2014-03-24 14:25:11 -0700

    divide two integer

--- LC/LC_divideTwoInt.py
+++ LC/LC_divideTwoInt.py
@@ -7,21 +7,35 @@ Created on Jan 14, 2014
 @author: Songfan
 '''
 
-''' algorithm: without mul, div, mod, we are left with bit manipulation, add, subtract '''
+''' algorithm: without mul, div, mod, we are left with bit manipulation, add, subtract 
+    double the divisor and compared with dividend, update result
+
+'''
 
 def divide(a, b):
-    assert(b != 0), 'divide by zero!'
+    if b == 0: return None
     if a == 0: return 0
-    if (a > 0 and b > 0) or (a < 0 and b < 0): sign = 1
-    elif (a < 0 and b > 0) or (a > 0 and b < 0): sign = -1
     
-    res, tmp = -1, 0
-    while tmp <= a:
-        res += 1
-        tmp += b
+    # init
+    if (a > 0 and b > 0) or (a < 0 and b < 0): pos = True
+    else: pos = False
+    dividend = a if a > 0 else -a
+    divisor = b if b > 0 else -b
+
+    res = dividePos(dividend, divisor)
+    return res if pos else -res
     
-    if sign == 1: return res
-    else: return -res
+def dividePos(a, b):
+    if a < b: return 0
+    res, e, divisor = 0, 0, b
+    
+    while a >= b:
+        res += 1<<e
+        a -= b
+        b <<= 1
+        e += 1
+    return res + dividePos(a, divisor) if a > 0 else res
+    
         
 print divide(0,3),'should be 0'       
 print divide(12,3),'should be 4'
